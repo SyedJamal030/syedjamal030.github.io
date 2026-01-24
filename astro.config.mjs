2b1b3edb555387167ae4fbd9e3c44653dc357380
Syed Jamal <smjwsb@gmail.com>
2026-01-24 14:02:55 +0500

    chore: rename site to root domain and update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://syedjamal030.github.io',
-  base: 'portfolio',
   vite: {
     plugins: [tailwindcss()],
   },
