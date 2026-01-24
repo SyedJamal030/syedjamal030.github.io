2852f4e3662c1487450735a27ebac8438996c59d
Syed Jamal <smjwsb@gmail.com>
2026-01-24 16:27:48 +0500

    feat: added seo configurations, and personalized favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://syedjamal030.github.io',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
